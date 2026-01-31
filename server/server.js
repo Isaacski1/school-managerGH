b6bedb01aabb33abc637bf1019d4830f53a5063a
Isaac Krampah <isaacskikrams@gmail.com>
2026-01-31 10:52:10 +0000

    Fix Render start script

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
 import express from "express";
 import admin from "firebase-admin";
 import cors from "cors";
 import dotenv from "dotenv";
 
-dotenv.config({ path: ".env.local" });
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, ".env.local") });
+dotenv.config({ path: path.join(__dirname, ".env") });
 
 const app = express();
 app.use(cors());
